fcf69a9db3f181a2b747673528d19000438be82a
polinajakovleva1 <117500741+polinajakovleva1@users.noreply.github.com>
2023-02-13 13:59:51 +0500

    Add files via upload

---
+++ Prprp.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace KomponentnoeLaba1
+{
+    [Serializable]
+    public class Prprp
+    {
+        [JsonProperty("nazvanie")]
+        public string nazvanie { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public Prprp()
+        {
+        }
+        public Prprp(int x, int y, int z, string n)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+            nazvanie = n;
+        }
+        public Prprp(int x, int y, string n)
+        {
+            X = x;
+            Y = y;
+            nazvanie = n;
+        }
+        public override string ToString()
+        {
+            return string.Format($"{nazvanie}");
+        }
+    }
+}
